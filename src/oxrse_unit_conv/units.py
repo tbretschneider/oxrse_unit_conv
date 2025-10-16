f69b481c9422a2417d7165bc5fab0d9f4c820893
Jakob Hartmann <hartmann.jakob@web.de>
2025-10-16 15:50:44 +0100

    feat: Add day (d) unit for time conversions (#7)

--- src/oxrse_unit_conv/units.py
+++ src/oxrse_unit_conv/units.py
@@ -8,6 +8,9 @@ minute = Unit(name='minute', abbr='min', si=second, to_si_fun=lambda n: n * 60)
 hour = Unit(name='hour', abbr='h', si=second, to_si_fun=lambda n: n * 3600)
 h = hour
 
+day = Unit(name='day', abbr='d', si=second, to_si_fun=lambda n: n * 86400)
+d = day
+
 # meter
 kilometer = Unit(name='kilometer', abbr="km", si=meter, to_si_fun=lambda n: n * 1000)
 km = kilometer
